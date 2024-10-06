6ffe48f11a32f9eb469a56b2a80033b7f1a65675
zuifiti <chelmizinho@gmail.com>
2024-10-06 00:53:42 -0300

    feat: add sidebar-items icons

--- src/components/site/shared/sidebar/sidebar.tsx
+++ src/components/site/shared/sidebar/sidebar.tsx
@@ -2,6 +2,13 @@
 import { GoRocket } from "react-icons/go";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+
+import { MdInstallDesktop } from "react-icons/md";
+import { LuCheckSquare, LuHaze, LuListChecks, LuPalette, LuTextCursorInput, LuToggleRight } from "react-icons/lu";
+import { IoMdRadioButtonOn } from "react-icons/io";
+import { BsMenuButtonFill } from "react-icons/bs";
+import { RxButton } from "react-icons/rx";
+import { RiSparkling2Line } from "react-icons/ri";
 interface ISidebarSubitem {
     id: string;
     title: string;
@@ -20,81 +27,87 @@ const sidebarItems: ISidebarItem[] = [
     {
         id: "getting-started",
         title: "Getting Started",
-        Icon: GoRocket ,
+        Icon: GoRocket,
         subItems: [
             {
                 id: "introduction",
                 title: "Introduction",
                 href: "/docs/getting-started/introduction",
-                icon: GoRocket 
+                icon: LuHaze
             },
             {
                 id: "installation",
                 title: "Installation",
                 href: "/docs/getting-started/installation",
-                icon: GoRocket 
+                icon: MdInstallDesktop
             },
             {
                 id: "theming",
                 title: "Theming",
                 href: "/docs/getting-started/theming",
-                icon: GoRocket 
+                icon: LuPalette
             }
         ]
     },
     {
         id: "forms",
         title: "Forms",
-        Icon: GoRocket ,
+        Icon: GoRocket,
         subItems: [
             {
                 id: "input",
                 title: "Input",
                 href: "/docs/components/input",
-                icon: GoRocket 
+                icon: LuTextCursorInput
+            },
+            {
+                id: "multi-select",
+                title: "Multi Select",
+                href: "/docs/components/multi-select",
+                icon: LuListChecks
             },
             {
                 id: "select",
                 title: "Select",
                 href: "/docs/components/select",
-                icon: GoRocket 
+                icon: BsMenuButtonFill
             },
             {
                 id: "checkbox",
                 title: "Checkbox",
                 href: "/docs/components/checkbox",
-                icon: GoRocket 
+                icon: LuCheckSquare
             },
             {
                 id: "radio",
                 title: "Radio",
                 href: "/docs/components/radio",
-                icon: GoRocket 
+                icon: IoMdRadioButtonOn
             },
             {
                 id: "switch",
                 title: "Switch",
                 href: "/docs/components/switch",
-                icon: GoRocket 
+                icon: LuToggleRight
             },
         ]
     },
     {
         id: "buttons",
         title: "Buttons",
-        Icon: GoRocket ,
+        Icon: GoRocket,
         subItems: [
             {
                 id: "button",
                 title: "Button",
                 href: "/docs/components/button",
-                icon: GoRocket 
+                icon: RxButton
             },
             {
                 id: "rainbow-button",
                 title: "Rainbow Button",
                 href: "/docs/components/rainbow-button",
-                icon: GoRocket 
+                icon: RiSparkling2Line  
             }
         ]
     }
@@ -123,39 +136,38 @@ export default function Sidebar() {
         <div className="flex flex-col h-[calc(100dvh-5rem)] fixed w-80 max-w-80 bg-background p-5" >
             <div className="flex flex-col gap-5 w-full">
                 {sidebarItems.map((item) => (
-                <div className="flex flex-col gap-2 w-full" key={item.id}>
-                    <div className="flex flex-row items-center gap-2">
-                        <item.Icon className="text-primary" />
-                        <h2>{item.title}</h2>
+                    <div className="flex flex-col gap-2 w-full" key={item.id}>
+                        <div className="flex flex-row items-center gap-2">
+                            <item.Icon className="text-primary" />
+                            <h2>{item.title}</h2>
+                        </div>
+                        <div className="flex flex-col gap-2 w-full">
+                            {item.subItems.map((subItem) => (
+                                <motion.button
+                                    onHoverStart={() => handleHoverStart(subItem.id)}
+                                    onHoverEnd={() => handleHoverEnd(subItem.id)}
+                                    className={`group relative px-3 py-0 flex flex-row items-center gap-2 w-full justify-start duration-200 transition-colors ${hoveredItem === subItem.id ? 'text-foreground' : 'text-muted-foreground'
+                                        }`}
+                                    key={subItem.id}
+                                >
+                                    <subItem.icon className="w-4 h-4 text-foreground" />
+                                    <h2>{subItem.title}</h2>
+                                    <AnimatePresence>
+                                        {hoveredItem === subItem.id && (
+                                            <motion.span
+                                                layoutId="nav-item"
+                                                className="absolute bg-card inset-0 rounded-lg w-full h-full -z-10"
+                                                initial={{ opacity: 0 }}
+                                                animate={{ opacity: 1 }}
+                                                exit={{ opacity: 0 }}
+                                                transition={{ duration: 0.2 }}
+                                            />
+                                        )}
+                                    </AnimatePresence>
+                                </motion.button>
+                            ))}
+                        </div>
                     </div>
-                    <div className="flex flex-col gap-2 w-full">
-                        {item.subItems.map((subItem) => (
-                            <motion.button 
-                            onHoverStart={() => handleHoverStart(subItem.id)}
-                            onHoverEnd={() => handleHoverEnd(subItem.id)}
-                            className={`group relative px-3 py-0 flex flex-row items-center gap-2 w-full justify-start duration-200 transition-colors ${
-                                hoveredItem === subItem.id ? 'text-foreground' : 'text-muted-foreground'
-                            }`}
-                            key={subItem.id}
-                            >
-                                <subItem.icon className="w-4 h-4 text-foreground" />
-                                <h2>{subItem.title}</h2>
-                                <AnimatePresence>
-                                    {hoveredItem === subItem.id && (
-                                        <motion.span 
-                                        layoutId="nav-item"
-                                        className="absolute bg-card inset-0 rounded-lg w-full h-full -z-10"
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        exit={{ opacity: 0 }}
-                                        transition={{ duration: 0.2 }}
-                                        />
-                                    )}
-                                </AnimatePresence>
-                            </motion.button>
-                        ))}
-                    </div>
-                </div>
                 ))}
             </div>
         </div>
