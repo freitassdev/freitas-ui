02aa7294af7240021d5a81834932bd979569bfdc
zuifiti <chelmizinho@gmail.com>
2024-10-06 22:45:06 -0300

    fix: padding-x in mobile

--- src/app/docs/components/template.tsx
+++ src/app/docs/components/template.tsx
@@ -8,7 +8,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
             <div className="flex flex-row w-full h-full mx-auto mt-20 max-md:w-full max-md:px-0 md:w-[800px] lg:w-[1000px] xl:w-[1300px] 2xl:w-[1500px] overflow-hidden">
                 <Sidebar />
                 <div className="flex-1 ml-80 max-md:ml-0 h-full">
-                    <div className="h-full overflow-y-auto px-5 py-3">
+                    <div className="h-full overflow-y-auto px-5 py-3 max-sm:px-2">
                         {children}
                     </div>
                 </div>
